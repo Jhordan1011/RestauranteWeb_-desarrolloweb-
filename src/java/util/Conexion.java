b603d15a9e7ff468fa5c31b4c3d0bfcf4b404d70
egusquiza31 <egusquiza.31@hotmail.com>
2025-07-09 12:51:57 -0500

    crud platos

--- src/java/util/Conexion.java
+++ src/java/util/Conexion.java
@@ -13,7 +13,7 @@ public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/restaurante_simple?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
 
-    private static final String CLAVE = "ajugardota2";
+    private static final String CLAVE = "contraseña";
 
 
     public static Connection getConnection() {
